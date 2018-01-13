28f69c8780a8de3d7a87e67e8ebb5a7d97d582c5
ryogam <garyo08200820@gmail.com>
2018-01-13 18:06:05 +0900

    [add] add ncbi_checker function

--- FASTASearch.py
+++ FASTASearch.py
@@ -31,7 +31,11 @@ def argparser():
 def makesoup(url):
     with urllib.request.urlopen(url) as response:
         html = response.read()
-        soup = BeautifulSoup(html, "lxml")
+        time.sleep(5)
+        try:
+            soup = BeautifulSoup(html, "lxml")
+        except:
+            soup = BeautifulSoup(html, "html5lib")
     return (soup)
 
 def makesoup_java(url):
@@ -39,22 +43,21 @@ def makesoup_java(url):
     driver.get(url)
     time.sleep(5)
     html = driver.page_source.encode('utf-8')  # more sophisticated methods may be available
-    soup_java= BeautifulSoup(html, 'lxml')
+    try:
+        soup_java = BeautifulSoup(html, "lxml")
+    except:
+        soup_java = BeautifulSoup(html, "html5lib")
     return(soup_java)
     
 def url_ncbi(genename, kind):
     url = 'https://www.ncbi.nlm.nih.gov/gene/?term=' + genename + '+' + kind
-    try:
-        soup = makesoup(url)
-    except:
-        print('[ERROR] Something error occered')
-        sys.exit()
+    soup = makesoup(url)
     if  'The following term was not found in Gene' in str(soup.findAll('span', class_='icon')):
-        print('[ERROR] NO matched Gene is found')
+        print('[ERROR] The following term was not found in Gene')
         sys.exit()
 
     elif 'No items found.' in str(soup.findAll('span', class_='icon')) :
-        print('[ERROR] No matced item is found')
+        print('[ERROR] No items found.')
         sys.exit()
     else :
         pass
@@ -63,32 +66,34 @@ def url_ncbi(genename, kind):
         ncbiurl = url
     else:
         hrefname = re.compile('/gene/\w')
-        atag = soup.findAll('a', href=hrefname)[1]
+        atag = soup.findAll('a', href=hrefname)[2]
         href = str(atag).split(" ")[1]
         ncbiurl = 'https://www.ncbi.nlm.nih.gov' + href.split('"')[1]
     return(ncbiurl)
 
-def url_ncbi_checker(ncbiurl, genename):        
-    soup = makesoup(ncbiurl)
-    dd = soup.findAll('dd', class_="noline")
-    Egenename = mdd.split('<')[0]
-    AlsoKnown = str(soup.findAll('td')[3]).replace('<', '>').split('>')[2]
 
+def ncbi_soup(ncbiurl):
+    ncbisoup = makesoup(ncbiurl)
+    return(ncbisoup)
+
+def ncbi_checker(ncbisoup, genename):    
+    Egenename = str(ncbisoup.find('dl', id="summaryDl").find('dd', class_='noline')).replace('<', '>').split('>')[2]    
     if Egenename == genename:
-        print('the input genename is same with Official Symbol')
-    else:
-        print('the Official Symbol of the input genename is ' + Egenename +', founded' )
+        print('The input genename is same with Official Symbol name')
 
-
-def fasta_from_ncbi(ncbiurl):
-    soup = makesoup(ncbiurl)
-    fastaurl = 'https://www.ncbi.nlm.nih.gov' + str(soup.findAll('a', title="Nucleotide FASTA report")[0]).split('"')[1]
+    if not Egenename == genename:
+        for line in ncbisoup.find('dl', id="summaryDl").findAll('dd'):
+            if '; ' in str(line):
+                if genename in str(line).replace('<', '>').replace('>', ' ').replace(';', '').split(' '):
+                    print('the Official Symbol of the input genename is ' + Egenename +', founded' )
+    
+def fasta_from_ncbi(ncbisoup):
+    fastaurl = 'https://www.ncbi.nlm.nih.gov' + str(ncbisoup.findAll('a', title="Nucleotide FASTA report")[0]).split('"')[1]
     return(fastaurl)
 
 def seq_fasta(fastaurl, genename, kind):
     fastasoup = makesoup_java(fastaurl)
     comment = '> '+ str(fastasoup.findAll('pre')).split(';')[1].split('\n')[0]
-
     f = open('output.txt',  'w')
     f.write(comment + '\n')
     for line in fastasoup.findAll('span', class_="ff_line" ):
@@ -101,6 +106,10 @@ if __name__ == '__main__' :
 
     ncbiurl = url_ncbi(genename, kind)
 
-    fastaurl = fasta_from_ncbi(ncbiurl)
+    ncbisoup = ncbi_soup(ncbiurl)
+
+    ncbi_checker(ncbisoup, genename)
+
+    fastaurl = fasta_from_ncbi(ncbisoup)
 
     seq_fasta(fastaurl, genename , kind)
